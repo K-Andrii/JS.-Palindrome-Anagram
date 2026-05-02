820e038b0505ef639f7010c8b796b957b36f0ff1
Andrii Kaleniuk <and.kaleniuk@gmail.com>
2026-05-02 22:38:58 +0300

    feat: palindrome both option

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 "use strict";
 
+function isPalindromeEasy(str){
+    let reversedStr = str.split('').reverse().join('')
+    return reversedStr.toLowerCase() === str.toLowerCase();
+}
 
+function isPalindromeHard(str){
+    let lowerStr = str.toLowerCase();
+    for (let i = 0; i < Math.floor(str.length / 2); i++) {
+        if (lowerStr[i] !== lowerStr[str.length - 1 - i]) return false;
+    }
+    return true;
+}
 
+function isAnagramEasy(str){
 
+}
+
+function isAnagramHard(str){
+
+}
+
+console.log(isPalindromeEasy('Anna'))
+console.log(isPalindromeHard('Annaa'))
+console.log(isAnagramEasy())
+console.log(isAnagramHard())
